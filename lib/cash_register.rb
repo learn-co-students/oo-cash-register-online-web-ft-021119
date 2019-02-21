77339adbafb12ba4be8807b7400882f443b73f4a
Joan Lyness <joanrigdon@gmail.com>
2019-02-21 00:50:02 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :previous_total, :price
+
+  def initialize
+    @CashRegister = CashRegister.new
+    @previous_total = 0
+    @total = 0
+  end
+
+  def discount
+    @discount
+  end
+
+  def add_item(title, price, quantity=0)
+    @total = @previous_total + (price*quantity)
+    @total
+  end
+
+end
